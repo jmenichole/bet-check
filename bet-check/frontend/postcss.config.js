3cfc142b9586b8a3f4cb5b63c5957b42bbbcd9c0
Jamie <j.chapman7@yahoo.com>
2025-12-11 02:51:33 -0600

    Add PostCSS configuration for Tailwind CSS compilation

---
+++ bet-check/frontend/postcss.config.js
@@ -0,0 +1,14 @@
+/**
+ * PostCSS Configuration - Tailwind CSS setup
+ * 
+ * Copyright (c) 2025 Jamie McNichol
+ * Licensed under MIT License
+ * https://jmenichole.github.io/Portfolio/
+ */
+
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+}
